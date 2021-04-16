18a6b5ee010d4650510b212b0714c8a31dbe586e
Imran Mohamed <ibm78@icloud.com>
2021-04-16 16:06:20 +0100

    LINKED LIST

---
+++ fast-and-slow-Pointers/linkedListCycleLength.js
@@ -0,0 +1,33 @@
+class Node{
+    constructor(value, next = null){
+        this.value = value
+        this.next = next
+    }
+}
+
+function findCycleLength(head){
+    let slow = head,
+        fast = head
+
+    while(fast!==null && fast.next!==null){
+        fast = fast.next.next // hare
+        slow = slow.next //tortoise
+        if(slow==fast) return calculateCycleLength(slow)
+    }
+    return false
+}
+
+
+function calculateCycleLength(slow){
+    let current = slow, 
+        cycle_length = 0
+
+    while(true){
+        current = current.next
+        cycle_length++
+        if(current===slow) break
+    }
+    
+    return cycle_length
+}
+//O(N)|O(1)
