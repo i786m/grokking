161b543aaae7b06ccea3ea2934694abe50926e71
Imran Mohamed <ibm78@icloud.com>
2021-04-22 16:31:45 +0100

    binary tree path (DFS)

---
+++ tree-depth-first-search/binaryTreePath.js
@@ -0,0 +1,17 @@
+class TreeNode{
+    constructor(val ,left=null , right = null){
+        this.val = val
+        this.right = right
+        this.left = left
+    }
+}
+
+function hasPath(root, sum){
+    if(root==null) return false
+
+    if(root.val ===  sum && root.left === null && root.right === null) return true
+
+    return hasPath(root.left, sum-root.val)||hasPath(root.right, sum-root.val)
+} 
+
+//O(N)|O(N)
