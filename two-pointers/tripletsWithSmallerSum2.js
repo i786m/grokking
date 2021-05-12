c9e8e4fb88b5fc74ea0d47e25060a8080a988cda
Imran Mohamed <ibm786@icloud.com>
2021-05-12 15:24:14 +0100

    triplets with smaller sum 2

--- two-pointers/tripletsWithSmallerSum2.js
+++ two-pointers/tripletsWithSmallerSum2.js
@@ -1,21 +1,22 @@
 function tripletWithSmallerSum(arr,target){
     arr.sort((a,b)=> a - b)
-    let count = 0
+    let triplets = []
     for(let i = 0 ; i< arr/length -2; i++){
-        count += searchPair(arr, target - arr[i], i)
+        (arr, target - arr[i], i,triplets)
     }
-    return count
+    return triplets
 }
 
 
-function searchPair(arr, targetSum , first){
-    let count = 0,
-        left = first + 1 , 
+function searchPair(arr, targetSum , first, triplets){
+    let left = first + 1 , 
         right = arr.length - 1
     while(left < right){
         if(arr[left] + arr[right] < targetSum){ // found the triplet
             //since arr[right]>=arr[left], therefore, we can replace arr[right] by any num between left and right to get a sum less than target sum
-            count += right -left
+            for(let i = right ; i>left ; i--){
+                triplets.push([arr[first], arr[left], arr[i]])
+            }
             left++
         }else{
             right-- // we need a pair with a smaller sum
