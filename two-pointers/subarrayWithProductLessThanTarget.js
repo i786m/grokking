1611df1f5f3c4aee9b62a1eef779799f43ac825d
Imran Mohamed <ibm786@icloud.com>
2021-05-12 15:45:55 +0100

    subarray with prouct less than a target

---
+++ two-pointers/subarrayWithProductLessThanTarget.js
@@ -0,0 +1,21 @@
+const Deque = require('./collections/deque') //collectionsjs.com
+
+function findSubarray(arr, target){
+    let res = [] , 
+        prod = 1 ,
+        left = 0
+    for(let right = 0 ; right < arr.length ; right++){
+        prod *= arr[right]
+        while(prod >= target && left < arr.length){
+            prod /= arr[left]
+            left++
+        }
+        // since the prod of all numbers from l to r is less than the target therefore all subarrays from l to r will have a prod less than target too  ; to avoid duplicates, we will start with the subarray containing only arr[right] then extend it
+        const templist = new Deque()
+        for(let i = right ; i > left - 1; i--){
+            templist.unshift(arr[i])
+            res.push(templist.toArray())
+        }
+    }
+    return res
+}
