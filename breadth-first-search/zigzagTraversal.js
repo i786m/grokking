a3e9630f0d8720392cf15a254530f01fb8ae49e4
Imran Mohamed <ibm786@icloud.com>
2021-05-18 16:17:15 +0100

    added the question problem for zigzag traversal

--- breadth-first-search/zigzagTraversal.js
+++ breadth-first-search/zigzagTraversal.js
@@ -1,3 +1,5 @@
+//Given a binary tree, populate an array to represent its zigzag level order traversal. You should populate the values of all nodes of the first level from left to right, then right to left for the next level and keep alternating in the same manner for the following levels.
+
 const Deque = require('./collections/deque') // collectionsjs.com
 
 class TreeNode{
@@ -26,7 +28,8 @@ function traverse(root){
             //add node to current lvl based on traverse direction
             leftToRight? currentLevel.push(currentNode.val) 
                         :currentLevel.unshift(currentNode.val)
-
+            
+            //insert children of current node to the q
             if(currentNode.left !== null){
                 q.push(currentNode.left)
             }
